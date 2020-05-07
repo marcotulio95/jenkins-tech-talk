12cc5c4f139b5505bdc7e8a133611d816ef2fc82
Marco Tulio <marco.tulio95@hotmail.com>
2020-05-07 01:49:18 -0300

    Fixing Jenkinsfile

--- jenkinsfile.js
+++ jenkinsfile.js
@@ -1,36 +1,37 @@
+def runTests = true;
 currentBuild.result = 'SUCCESS';
-pipeline{
+
+
+node('master') {
 	
-	agent any
-    
-	tools {
+    tools {
         maven 'Maven 3.3.9'
         jdk 'jdk8'
     }
+	
 
-	 runTests = true;
-	 isMaster = false;
-     isRelease = false;
-     isFeature = false;
+	def isMaster = false;
+    def isRelease = false;
+    def isFeature = false;
     
     try
     {
-		 workspace = env.WORKSPACE;
-		 jenkins_home = env.JENKINS_HOME;
-    	 branchName = env.BRANCH_NAME;
+		def workspace = env.WORKSPACE;
+		def jenkins_home = env.JENKINS_HOME;
+    	def branchName = env.BRANCH_NAME;
 	    isMaster = branchName.toString() == "master";
-	     isDevelop = branchName.toString() == "develop";
+	    def isDevelop = branchName.toString() == "develop";
 	    isRelease = isReleaseBranch();
 	    isFeature = !(isMaster || isRelease || isDevelop);
-	     jobName = env.JOB_NAME;
-	     buildNumber = env.BUILD_NUMBER;
+	    def jobName = env.JOB_NAME;
+	    def buildNumber = env.BUILD_NUMBER;
 
 	    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '10', daysToKeepStr: '', numToKeepStr: '')), disableConcurrentBuilds(), pipelineTriggers([cron('H H(0-0) * * *')])])
 
 		echo("Branch: $branchName; master: $isMaster release: $isRelease develop: $isDevelop")
     	setBuildName()
     	sendMessageViaSlack("Build Started - ${env.JOB_NAME}  (<${env.BUILD_URL}|Open>)")
-
+		
 		stage('Checkout & CleanUp'){
 			deleteDir()
 			checkoutFomGit()
@@ -70,36 +71,36 @@ pipeline{
 
 //Myfunctions
 
- sendMessageViaSlack(message){
+def sendMessageViaSlack(message){
 
-	 channel = "teste-techtalk"
+	def channel = "teste-techtalk"
 
 	slackSend channel: "$channel", message: "$message", tokenCredentialId: 'AtZTUdI8yhYkGr1LNcMbChWe'
 }
 
- checkoutFomGit(){
+def checkoutFomGit(){
 		checkout scm
 }
 
- getRepository(gitUrl, folderTarget){
+def getRepository(gitUrl, folderTarget){
 	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "$folderTarget"]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'b0864a31-0a3a-48fd-be36-8f15e5d0665a', url: "$gitUrl" ]]])
 }
 
 
- getRepository(gitUrl, folderTarget, branch){
+def getRepository(gitUrl, folderTarget, branch){
     checkout([$class: 'GitSCM', branches: [[name: "*/$branch"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "$folderTarget"]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'b0864a31-0a3a-48fd-be36-8f15e5d0665a', url: "$gitUrl" ]]])
 }
 
 boolean isReleaseBranch()
 {
-     branchName = env.BRANCH_NAME
+    def branchName = env.BRANCH_NAME
     // release branches named major.minor(.bugfix)_releaseName; note: env.BRANCH_NAME is available in multi-branch-pipeline projects
     // Gitflow forces us to use prefix "release/"
     branchName != null && branchName.matches("(release/)?\\d+\\.\\d+(\\.\\d+)?_.*");
 }
 
- setBuildName(){
-	 jobName = env.JOB_NAME
-	 buildNumber = currentBuild.number
+def setBuildName(){
+	def jobName = env.JOB_NAME
+	def buildNumber = currentBuild.number
 	currentBuild.displayName = "$jobName"+"#"+"$buildNumber"
 }
