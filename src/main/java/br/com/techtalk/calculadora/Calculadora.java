82bdeba016d107bde5b3f4ebc7723507a1c28c70
Marco Tulio <marco.tulio95@hotmail.com>
2020-05-07 01:20:05 -0300

    Adding mvn java project

---
+++ src/main/java/br/com/techtalk/calculadora/Calculadora.java
@@ -0,0 +1,9 @@
+package br.com.techtalk.calculadora;
+
+public interface Calculadora {
+
+    Double somar(Double number1, Double number2);
+    Double subtrair(Double number1, Double number2);
+    Double multiplicar(Double number1 , Double number2);
+    Double dividir(Double number1, Double number2);
+}
---
+++ src/test/java/br/com/techtalk/AppTest.java
@@ -0,0 +1,22 @@
+package br.com.techtalk;
+
+import static org.junit.Assert.assertTrue;
+
+import br.com.techtalk.calculadora.impl.CalculadoraImpl;
+import br.com.techtalk.calculadora.impl.CalculadoraImplTest;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
